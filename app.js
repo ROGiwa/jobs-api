c89694e733412f3c7b54445c364ed9275e8e5078
rilwangiwa <giwarilwan00@gmail.com>
2024-02-09 01:48:25 +0100

    update

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//connect DB
+const connect = require('./db/connect');
+//routes
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 // extra packages
-
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,9 +26,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    await connect();
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
