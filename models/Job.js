f46e2271c6e5898bcba555c5fe73b57b6ea009cd
rilwangiwa <giwarilwan00@gmail.com>
2024-02-15 02:30:48 +0100

    update

--- models/Job.js
+++ models/Job.js
@@ -1,17 +1,29 @@
 const mongoose = require('mongoose');
 
-const JobSchema = new mongoose.Schema({
-  company: {
-    typeof: 'string',
-    required: [true, 'Please provide a company'],
-    maxLength: 50,
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide a company'],
+      maxLength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide a position'],
+      maxLength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide a User'],
+    },
   },
-  position: {
-    typeof: 'string',
-    required: [true, 'Please provide a position'],
-    maxLength: 100,
-  },
-  status: {
-    type: 'string',
-  },
-});
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Job', JobSchema);
