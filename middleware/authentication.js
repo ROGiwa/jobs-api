dfd47275edfaf4ced400d7419cad21cd0edeac77
rilwangiwa <giwarilwan00@gmail.com>
2024-02-16 03:54:18 +0100

    update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
   //check header
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return new UnauthenticatedError('Authentication invalid');
+    throw new UnauthenticatedError('Authentication invalid');
   }
 
   const token = authHeader.split(' ')[1];
--- routes/jobs.js
+++ routes/jobs.js
@@ -12,5 +12,3 @@ const {
 router.route('/').post(createJob).get(getAllJobs);
 router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob);
 module.exports = router;
-
-module.exports = router;
